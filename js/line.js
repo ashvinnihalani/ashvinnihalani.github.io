ff27e2dde5b0904cb1b9bc61961b23805ff34677
Ashvin Nihalani <ashvin.nihalani@gmail.com>
2017-07-07 21:17:26 -0700

    Initial Animations

--- js/line.js
+++ js/line.js
@@ -5,30 +5,47 @@ mainCanvas.width = window.innerWidth;
 mainCanvas.height = window.innerHeight;
 var canvasWidth = mainCanvas.width;
 var canvasHeight = mainCanvas.height;
-var requestAnimationFrame = window.requestAnimationFrame || 
-                            window.mozRequestAnimationFrame || 
-                            window.webkitRequestAnimationFrame || 
-                            window.msRequestAnimationFrame;
-if (canvasHeight< canvasWidth){ //Laptops etc 
+var requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
+var spacing = canvasWidth/100*5
+console.log("Setup Done");
+//Set up for drawing Blue Line
+var tempy = -10;
+var tempx = canvasWidth*.6;
+var speed = 100; //Change this for longer animations
+var yslope = canvasHeight/speed;
+var xslope = canvasWidth*.3/speed;
+
+function drawLines(){
+	mainContext.clearRect(0,0,canvasWidth,canvasHeight);
 	mainContext.beginPath();
 	mainContext.moveTo(canvasWidth*.6, -10);
-	mainContext.lineTo(canvasWidth*.3, canvasHeight+10);
+	mainContext.lineTo(tempx, tempy);
 	mainContext.lineWidth = canvasWidth/100;
 	mainContext.strokeStyle = 'blue';
 	mainContext.stroke();
 	mainContext.closePath();
 	mainContext.beginPath();
-	mainContext.moveTo(canvasWidth*.6+canvasWidth/100*5, -10);
-	mainContext.lineTo(canvasWidth*.3+canvasWidth/100*5, canvasHeight+10);
+	mainContext.moveTo(canvasWidth*.6+spacing, -10);
+	mainContext.lineTo(tempx+spacing, tempy);
 	mainContext.lineWidth = canvasWidth/100;
-	mainContext.strokeStyle = '#ff0000';
+	mainContext.strokeStyle = 'red';
 	mainContext.stroke();
 	mainContext.closePath();
 	mainContext.beginPath();
-	mainContext.moveTo(canvasWidth*.6+2*canvasWidth/100*5, -10);
-	mainContext.lineTo(canvasWidth*.3+2*canvasWidth/100*5, canvasHeight+10);
+	mainContext.moveTo(canvasWidth*.6+spacing*2, -10);
+	mainContext.lineTo(tempx+spacing*2, tempy);
 	mainContext.lineWidth = canvasWidth/100;
-	mainContext.strokeStyle = '#00ff00';
+	mainContext.strokeStyle = 'green';
 	mainContext.stroke();
 	mainContext.closePath();
+	if(tempy<canvasHeight+10 && tempx>canvasHeight*.3){
+		tempy+=yslope;
+		tempx-=xslope;
+		requestAnimationFrame(drawLines);
+	}
+	else{
+		
+	}
+	
 }
+drawLines();
